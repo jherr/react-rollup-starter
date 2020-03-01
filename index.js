c78b3cf00ff82990be8ba19a5d56f3b4e9a9126f
Jack Herrington <jherr@pobox.com>
2020-03-01 08:16:17 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    Hi there, I'm React from Rollup.
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('app'));
